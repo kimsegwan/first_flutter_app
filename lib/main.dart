daecfc08c03bf3ef966df3924b51fb0045244efd
kimsegwan <tprhks@nate.com>
2022-01-10 01:22:32 +0900

    AnimalItem

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import './animalItem.dart';
+
+//import 'package:flutter/services.dart';
 // import 'imageWidget.dart';
+import 'sub/firstPage.dart';
+import 'sub/secondPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,13 +34,40 @@ class WidgetApp extends StatefulWidget {
   _WidgetExampleState createState() => _WidgetExampleState();
 }
 
-class _WidgetExampleState extends State<WidgetApp> {
+class _WidgetExampleState extends State<WidgetApp>
+    with SingleTickerProviderStateMixin {
   String sum = '';
   TextEditingController value1 = TextEditingController();
   TextEditingController value2 = TextEditingController();
   List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
   List<DropdownMenuItem<String>> _dropDownMenuItems = [];
   String _buttonText = "";
+  TabController? controller;
+  List<Animal> animalList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var item in _buttonList) {
+      _dropDownMenuItems.add(DropdownMenuItem(child: Text(item), value: item));
+    }
+    _buttonText = _dropDownMenuItems[0].value.toString();
+    controller = TabController(length: 2, vsync: this);
+    controller!.addListener(() {
+      if(!controller!.indexIsChanging) {
+        print('이전 index, ${controller?.previousIndex}');
+        print('현재 index, ${controller?.index}');
+      }
+    });
+    animalList.add(Animal(animalName: '벌', kind: '곤충', imagePath: 'repo/images/bee.png', flyExist: false));
+    animalList.add(Animal(animalName: '고양이', kind: '포유류', imagePath: 'repo/images/cat.png', flyExist: false));
+    animalList.add(Animal(animalName: '젖소', kind: '포유류', imagePath: 'repo/images/cow.png', flyExist: false));
+    animalList.add(Animal(animalName: '강아지', kind: '포유류', imagePath: 'repo/images/dog.png', flyExist: false));
+    animalList.add(Animal(animalName: '여우', kind: '포유류', imagePath: 'repo/images/fox.png', flyExist: false));
+    animalList.add(Animal(animalName: '원숭이', kind: '영장류', imagePath: 'repo/images/monkey.png', flyExist: false));
+    animalList.add(Animal(animalName: '돼지', kind: '포유류', imagePath: 'repo/images/pig.png', flyExist: false));
+    animalList.add(Animal(animalName: '벌', kind: '늑대', imagePath: 'repo/images/woolf.png', flyExist: false));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +75,26 @@ class _WidgetExampleState extends State<WidgetApp> {
       appBar: AppBar(
         title: Text('Widget Example'),
       ),
-      body: Container(
+      body: TabBarView(
+        children: <Widget>[
+          FirstApp(list: animalList),
+          SecondApp(list: animalList)
+        ],
+        controller: controller,
+      ),
+      bottomNavigationBar: TabBar(tabs: <Tab>[
+        Tab(
+            icon: Icon(
+          Icons.looks_one,
+          color: Colors.blue,
+        )),
+        Tab(
+            icon: Icon(
+          Icons.looks_two,
+          color: Colors.blue,
+        )),
+      ], controller: controller,),
+      /*Container(
         padding: EdgeInsets.all(15),
         child: Center(
           child: Column(
@@ -75,11 +125,11 @@ class _WidgetExampleState extends State<WidgetApp> {
                       var value1Int = double.parse(value1.value.text);
                       var value2Int = double.parse(value2.value.text);
                       var result;
-                      if(_buttonText == '더하기') {
+                      if (_buttonText == '더하기') {
                         result = value1Int + value2Int;
-                      } else if(_buttonText == '빼기') {
+                      } else if (_buttonText == '빼기') {
                         result = value1Int - value2Int;
-                      } else if(_buttonText == '곱하기') {
+                      } else if (_buttonText == '곱하기') {
                         result = value1Int * value2Int;
                       } else {
                         result = value1Int / value2Int;
@@ -97,20 +147,17 @@ class _WidgetExampleState extends State<WidgetApp> {
                       });
                     },
                     value: _buttonText,
-                  ))
+                  )),
             ],
           ),
         ),
-      ),
+      ),*/
     );
   }
 
   @override
-  void initState() {
-    super.initState();
-    for (var item in _buttonList) {
-      _dropDownMenuItems.add(DropdownMenuItem(child: Text(item), value: item));
-    }
-    _buttonText = _dropDownMenuItems[0].value.toString();
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
---
+++ lib/sub/firstPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../animalItem.dart';
+
+class FirstApp extends StatelessWidget {
+  final List<Animal> list;
+  FirstApp({Key? key, required this.list}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
