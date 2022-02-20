ba4db9efe12f4d195ba739aca04fcd92b2b92eae
kimsegwan <tprhks@nate.com>
2022-02-20 23:47:20 +0900

    - 내비게이션 화명 이동 데이터 넘기기

--- lib/sub/sevenPage.dart
+++ lib/sub/sevenPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../seven/subDetail.dart';
+import '../seven/secondDetail.dart';
+import '../seven/thirdPage.dart';
 import '../animalItem.dart';
 
 // void main() {
@@ -7,68 +10,19 @@ import '../animalItem.dart';
 
 class SevenPage extends StatelessWidget {
   final List<Animal> list;
-
+  static const String _title = 'Widget Example';
   SevenPage({Key? key, required this.list}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SubPage Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatefulWidget {
-  @override
-  State createState() => _FirstPage();
-}
-
-class _FirstPage extends State<FirstPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sub Page Main'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text('첫 번째 페이지'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondPage()));
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Page'),
-      ),
-      body: Container(
-        child: Center(
-          child: Center(
-            child: RaisedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('돌아가기'),
-            ),
-          ),
-        ),
-      ),
+      title: _title,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SubDetail(),
+        '/second': (context) => SecondDetail(),
+        '/third': (context) => ThirdDetail(),
+      },
     );
   }
 }
